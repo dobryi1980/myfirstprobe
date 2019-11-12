92686e16b577ba800b14302cb2d668aa032939c9
dobryi1980 <dobryi1980dobryi@gmail.com>
2019-11-12 22:03:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Technologies from './Technologies.js';
+import Header from './Header.js';
+import './App.css';
+
+const App = () => {
+  return (
+    
+    <div>
+      <Technologies />
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header = () => {
+    return (
+      <div>
+        <a href='#s'> home</a>
+        <a href='#s'> Bome</a>
+        <a href='#s'> Dome</a>
+      </div>
+    );
+  }
+  export default Header;
